8716d08496ed68fac967bbdf0af91bd11edac4c9
ValeraPL <mafarawa5@gmail.com>
2019-07-31 13:49:19 +0300

    Переработана программа

--- Python/Lesson/main.py
+++ Python/Lesson/main.py
@@ -1,33 +1,95 @@
 arrayName = []
 tupleName = ()
-sizeArray = int(input("Введите размер массива >>>: "))
-info = """tuple: Поместить массив в кортеж\nlist: Просмотреть весь массив\nindex: Узнать индекс элемента\ncount: Узнать количество схожих элементов\npop: Удалить элемент по индексу\nclear: Отчистить весь массив"""
 
-for i in range(sizeArray):
-	arrayName.append(input("Введите имя >>>: "))
-
-print(info)
-
-querry = input(">>>: ")
-
-if querry == "index":
+def index():
 	request = input("Введите имя >>>: ")
 	print("Имя:", request, ",", "Индекс:", arrayName.index(request))
-elif querry == "count":
+
+def count():
 	request = input("Введите имя >>>: ")
 	print("Имя:", request, ",", "Количество:", arrayName.count(request))
-elif querry == "pop":
+
+def pop():
 	request = int(input("Введите индекс элемента >>>: "))
-	arrayName.pop(request)
-	print("Имя:", arrayName[request], "Удалено")
-elif querry == "clear":
+	print("Имя:", arrayName[request], "Удалить[Y/N]")
+	choice = input(">>>: ")
+	if choice == "y":
+		arrayName.pop(request)
+		print("Удалено")
+	elif choice == "n":
+		main()
+
+def clear():
 	arrayName.clear()
 	print("Весь массив отчищен")
-elif querry == "list":
-	for index in arrayName:
-		print(index)
-elif querry == "tuple":
+
+def list():
+	for i in arrayName:
+		print(i)
+
+def tup():
 	tupleName = arrayName
 	print("Массив помещен в кортеж")
-else:
-	print("Такой функции нет")
+
+def info():
+	commands = """
+		tuple: Поместить массив в кортеж
+		list: Просмотреть весь массив
+		index: Узнать индекс элемента
+		count: Узнать количество схожих элементов
+		pop: Удалить элемент по индексу
+		create: Создать массив
+		clear: Отчистить весь массив
+	"""
+
+	print(commands)
+
+def main():
+	querry = input(">>>: ")
+
+	if querry == "index":
+		index()
+		main()
+	elif querry == "count":
+		count()
+		main()
+	elif querry == "pop":
+		pop()
+		main()
+	elif querry == "clear":
+		clear()
+		main()
+	elif querry == "list":
+		list()
+		main()
+	elif querry == "tuple":
+		tup()
+		main()
+	elif querry == "info":
+		info()
+		main()
+	elif querry == "create":
+		createArray()
+	elif querry == "exit":
+		exit(0)
+	else:
+		print("Такой функции нет")
+		main()
+
+def createArray():
+	sizeArray = int(input("Введите размер массива >>>: "))
+
+	for i in range(sizeArray):
+		arrayName.append(input("Введите имя >>>: "))
+
+	for j in arrayName:
+		print(j)
+
+	choice = input("Все верно[Y/N]? >>>: ")
+	if choice == "n":
+		arrayName.clear()
+		createArray()
+	elif choice == "y":
+		main()
+
+createArray()
